df894f278c16db3cedac7199253601f9da417c2d
Pouya Ashna <pouyaashna@gmail.com>
2020-12-30 11:52:25 +0330

    add swagger

---
+++ src/main/java/ir/snapp/phonebook/configuration/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+package ir.snapp.phonebook.configuration;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@EnableSwagger2
+@Configuration
+@ConditionalOnProperty(
+        value = "swagger.enable",
+        havingValue = "true"
+)
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "PhoneBook",
+                "This is a project for the recruitment process of the snapp-box company ",
+                "Version 1.0.0",
+                null,
+                new Contact("Pouya Ashna", "", "pouyaashna@gmail.com"),
+                null,
+                null,
+                Collections.emptyList());
+    }
+}
